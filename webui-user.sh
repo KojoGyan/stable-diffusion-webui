dabdf8b6c6c1e0d05bdf3b4db8ce3b7a7703cc7d
KojoGyan <99098679+KojoGyan@users.noreply.github.com>
2024-01-31 00:53:13 +0000

    Update webui-user.sh
    only share and path arguments

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="-f --autolaunch --ngrok "2bgSFJcxfMsz8Pv9qTOGeWABfOi_6ZV27TAurGb6w2RoWUXRa" --lowvram --xformers --share --ckpt-dir "/kaggle/temp/models" --vae-path "/kaggle/working/stable-diffusion-webui/models/VAE"\ --enable-insecure-extension-access --no-half-vae"
+export COMMANDLINE_ARGS="--ngrok "2bgSFJcxfMsz8Pv9qTOGeWABfOi_6ZV27TAurGb6w2RoWUXRa" --share --ckpt-dir "/kaggle/temp/models" --vae-path '/kaggle/working/stable-diffusion-webui/models/VAE'"
 
 # python3 executable
 #python_cmd="python3"
